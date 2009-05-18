0c0a6a46195fd0466175a2b65bee9f6cf3b798b3
David Larlet <larlet@gmail.com>
2009-05-18 11:23:17 +0200

    Overwrite: now works with the location defined by the user, rather than forcing MEDIA_ROOT. Thanks Michael Elsd?rfer.

--- backends/overwrite.py
+++ backends/overwrite.py
@@ -4,16 +4,18 @@ from django.conf import settings
 from django.core.files.storage import FileSystemStorage
 
 class OverwriteStorage(FileSystemStorage):
+    """
+    Comes from http://www.djangosnippets.org/snippets/976/
+    (even if it already exists in S3Storage for ages)
+    
+    See also Django #4339, which might add this functionality to core.
+    """
     
     def get_available_name(self, name):
         """
         Returns a filename that's free on the target storage system, and
         available for new content to be written to.
-        
-        Comes from http://www.djangosnippets.org/snippets/976/
-        (even if it already exists in S3Storage for ages)
         """
-        # If the filename already exists, remove it as if it was a true file system
         if self.exists(name):
-            os.remove(os.path.join(settings.MEDIA_ROOT, name))
+            self.delete(name)
         return name
