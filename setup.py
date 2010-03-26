ac72eead9dd04e5ee7ce33a080e89189290e43c8
Rich Leland <rich@richleland.com>
2010-03-26 18:10:18 -0400

    Updated setup.py to use distribute.

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
+import os, distribute_setup
+distribute_setup.use_setuptools()
 from setuptools import setup, find_packages
  
 setup(
-    name='django-storages',
-    version='1.1',
-    description='Support for many storages (S3, MogileFS, etc) in Django.',
+    name = 'django-storages',
+    version = '1.1',
+    packages = find_packages(),
+    
     author='David Larlet',
     author_email='david@larlet.fr',
+    license = 'BSD',
+    description='Support for many storages (S3, MogileFS, etc) in Django.',
     url='http://code.welldev.org/django-storages/',
-    packages=find_packages(),
-    py_modules=['S3'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
@@ -18,11 +21,6 @@ setup(
         'Programming Language :: Python',
         'Framework :: Django',
     ],
-    # Make setuptools include all data files under version control,
-    # svn and CVS by default
-    include_package_data=True,
+    py_modules=['S3'],
     zip_safe=False,
-    # Tells setuptools to download setuptools_hg before running setup.py so
-    # it can find the data files under Hg version control.
-    setup_requires=['setuptools_hg'],
 )
