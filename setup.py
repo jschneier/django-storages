233c2d2e67f07ba37c0a886e45a326c6fa7c9b35
Rich Leland <rich@richleland.com>
2010-03-26 21:54:31 -0400

    Fixing PyPI install issue.

--- setup.py
+++ setup.py
@@ -4,15 +4,16 @@ from setuptools import setup, find_packages
  
 setup(
     name = 'django-storages',
-    version = '1.1',
+    version = '1.1.1',
     packages = find_packages(),
     
-    author='David Larlet',
-    author_email='david@larlet.fr',
+    author = 'David Larlet',
+    author_email = 'david@larlet.fr',
     license = 'BSD',
-    description='Support for many storages (S3, MogileFS, etc) in Django.',
+    description = 'Support for many storages (S3, MogileFS, etc) in Django.',
     url='http://code.welldev.org/django-storages/',
-    classifiers=[
+    download_url = "http://bitbucket.org/david/django-storages/get/1.1.1.tar.gz",
+    classifiers = [
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
@@ -21,6 +22,6 @@ setup(
         'Programming Language :: Python',
         'Framework :: Django',
     ],
-    py_modules=['S3'],
-    zip_safe=False,
+    py_modules = ['S3'],
+    zip_safe = False,
 )
