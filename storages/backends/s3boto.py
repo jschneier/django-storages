2f1bd69e2512e65611e60aae242ea3581a76037e
Ian Lewis <IanMLewis@gmail.com>
2010-07-02 22:11:25 +0900

    Fixed security issue where a user could create a path that would write to a file outside the base location defined by AWS_LOCATION.

--- storages/backends/s3boto.py
+++ storages/backends/s3boto.py
@@ -1,5 +1,4 @@
 import os
-import posixpath
 import mimetypes
 
 try:
@@ -10,7 +9,8 @@ except ImportError:
 from django.conf import settings
 from django.core.files.base import File
 from django.core.files.storage import Storage
-from django.core.exceptions import ImproperlyConfigured
+from django.core.exceptions import ImproperlyConfigured, SuspiciousOperation
+from django.utils.encoding import force_unicode
 
 try:
     from boto.s3.connection import S3Connection, S3ResponseError
@@ -39,6 +39,32 @@ GZIP_CONTENT_TYPES  = getattr(settings, 'GZIP_CONTENT_TYPES', (
 
 if IS_GZIPPED:
     from gzip import GzipFile
+
+def safe_join(base, *paths):
+    """
+    A version of django.utils._os.safe_join for S3 paths.
+
+    Joins one or more path components to the base path component intelligently.
+    Returns a normalized version of the final path.
+
+    The final path must be located inside of the base path component (otherwise
+    a ValueError is raised).
+    
+    Paths outside the base path indicate a possible security sensitive operation.
+    """
+    from urlparse import urljoin
+    base_path = force_unicode(base)
+    paths = map(lambda p: force_unicode(p), paths)
+    final_path = urljoin(base_path + ("/" if not base_path.endswith("/") else ""), *paths)
+    # Ensure final_path starts with base_path and that the next character after
+    # the final path is '/' (or nothing, in which case final_path must be
+    # equal to base_path).
+    base_path_len = len(base_path)
+    if not final_path.startswith(base_path) \
+       or final_path[base_path_len:base_path_len+1] not in ('', '/'):
+        raise ValueError('the joined path is located outside of the base path'
+                         ' component')
+    return final_path
 
 class S3BotoStorage(Storage):
     """Amazon Simple Storage Service using Boto"""
@@ -96,7 +122,10 @@ class S3BotoStorage(Storage):
         return os.path.normpath(name).replace('\\', '/')
 
     def _normalize_name(self, name):
-        return posixpath.join(self.location, name).lstrip('/')
+        try:
+            return safe_join(self.location, name).lstrip('/')
+        except ValueError:
+            raise SuspiciousOperation("Attempted access to '%s' denied." % name)
 
     def _compress_content(self, content):
         """Gzip a given string."""
