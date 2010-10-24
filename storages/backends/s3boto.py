51982fc931af65a432108964bb26162f0972783c
Ian Lewis <IanMLewis@gmail.com>
2010-10-24 20:01:04 +0900

    Fixed an import error with newer versions of Boto by importing S3ResponseError from boto.exception

--- storages/backends/s3boto.py
+++ storages/backends/s3boto.py
@@ -13,7 +13,8 @@ from django.core.exceptions import ImproperlyConfigured, SuspiciousOperation
 from django.utils.encoding import force_unicode, smart_str
 
 try:
-    from boto.s3.connection import S3Connection, S3ResponseError
+    from boto.s3.connection import S3Connection
+    from boto.exception import S3ResponseError
     from boto.s3.key import Key
 except ImportError:
     raise ImproperlyConfigured, "Could not load Boto's S3 bindings.\
