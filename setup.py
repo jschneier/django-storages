ce3c84c5365293b4c925967e99894594130ca523
Rich Leland <rich@richleland.com>
2011-08-11 10:44:29 -0400

    Bump version to 1.1.2.

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup, find_packages
 import storages
- 
+
 setup(
     name = 'django-storages',
     version = storages.__version__,
     packages = find_packages(),
-    
+
     author = 'David Larlet',
     author_email = 'david@larlet.fr',
     license = 'BSD',
     description = 'Support for many storages (S3, MogileFS, etc) in Django.',
     url='http://code.welldev.org/django-storages/',
-    download_url = "http://bitbucket.org/david/django-storages/get/tip.tar.gz",
+    download_url = "http://bitbucket.org/david/django-storages/get/1.1.2.tar.gz",
     classifiers = [
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
