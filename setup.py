24c1e4bb94dfc5e6276c3e31b790a53f9aa19b91
Rich Leland <rich@richleland.com>
2012-01-06 14:10:38 -0500

    Remove download_url from setup file.

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
 from setuptools import setup, find_packages
 import storages
- 
+
 setup(
     name = 'django-storages',
     version = storages.__version__,
     packages = find_packages(),
-    
+
     author = 'David Larlet',
     author_email = 'david@larlet.fr',
     license = 'BSD',
     description = 'Support for many storages (S3, MogileFS, etc) in Django.',
     url='http://code.welldev.org/django-storages/',
-    download_url = "http://bitbucket.org/david/django-storages/get/tip.tar.gz",
     classifiers = [
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
