68a3a2c76eecac6cf18bb691f161f2ad8e467c43
Chris Streeter <chris@chrisstreeter.com>
2013-02-09 08:22:28 -0800

    Prevent listing huge buckets on S3.
    Previously, in when `AWS_PRELOAD_METADATA` is True,
    `S3BotoStorage.entries` would list every file in the bucket. However,
    if there is a `location` specified on the storage, we don't use the
    whole storage bucket with that storage backend. So pass our `location`
    as the `prefix` to the call to `entries`.

--- storages/backends/s3boto.py
+++ storages/backends/s3boto.py
@@ -275,7 +275,7 @@ class S3BotoStorage(Storage):
         """
         if self.preload_metadata and not self._entries:
             self._entries = dict((self._decode_name(entry.key), entry)
-                                for entry in self.bucket.list())
+                                for entry in self.bucket.list(prefix=self.location))
         return self._entries
 
     def _get_access_keys(self):
