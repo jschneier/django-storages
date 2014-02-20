c7957523c6c02b4bba8b34ca0070067540daba21
Joshua "jag" Ginsberg <jag@flowtheory.net>
2014-02-20 11:27:46 -0700

    Add missing import for ImproperlyConfigured

--- storages/backends/mongodb.py
+++ storages/backends/mongodb.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
 from django.core.files.base import File
 from django.core.files.storage import Storage
 from django.db import connections
