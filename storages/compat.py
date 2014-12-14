c16765f9ead5c1f7c7304fcae64f4e634abb67be
Josh Schneier <josh.schneier@gmail.com>
2014-12-14 16:59:19 -0500

    minor whitespace nit

--- storages/compat.py
+++ storages/compat.py
@@ -4,4 +4,4 @@ from django.utils.six import BytesIO
 try:
     from django.utils.deconstruct import deconstructible
 except ImportError: # Django 1.7+ migrations
-    deconstructible = lambda klass, *args, **kwargs : klass
+    deconstructible = lambda klass, *args, **kwargs: klass
