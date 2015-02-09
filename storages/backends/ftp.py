ebab6b3636d40053c23f22053bd2053612ff1d11
Tadas Dailyda <tadas@dailyda.com>
2015-02-09 14:54:29 +0000

    fix exists in FTPStorage

--- storages/backends/ftp.py
+++ storages/backends/ftp.py
@@ -185,7 +185,7 @@ class FTPStorage(Storage):
     def exists(self, name):
         self._start_connection()
         try:
-            if os.path.basename(name) in self._connection.nlst(
+            if name in self._connection.nlst(
                 os.path.dirname(name) + '/'
             ):
                 return True
