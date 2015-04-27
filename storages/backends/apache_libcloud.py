1f8db9108f8f73ebc6d7fa0430bf0e4b17c31d93
Forrest Chromebook <forrestp@nvbots.com>
2015-04-27 18:04:21 -0400

    Initialize apache-libcloud driver with user_id and key as kwargs

--- storages/backends/apache_libcloud.py
+++ storages/backends/apache_libcloud.py
@@ -40,8 +40,8 @@ class LibCloudStorage(Storage):
 
             Driver = get_driver(provider_type)
             self.driver = Driver(
-                self.provider['user'],
-                self.provider['key'],
+                user_id=self.provider['user'],
+                key=self.provider['key'],
                 **extra_kwargs
             )
         except Exception as e:
