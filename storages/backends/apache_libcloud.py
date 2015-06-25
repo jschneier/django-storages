a0e0cdd55ffaec81f980ea6b20cb0a79e59baabd
Karl Hobley <karlhobley10@gmail.com>
2015-06-25 09:39:16 +0100

    Made LibCloudFile.file a property
    Fetch the file whenever the property is accessed.

--- storages/backends/apache_libcloud.py
+++ storages/backends/apache_libcloud.py
@@ -140,21 +140,30 @@ class LibCloudStorage(Storage):
 class LibCloudFile(File):
     """File inherited class for libcloud storage objects read and write"""
     def __init__(self, name, storage, mode):
-        self._name = name
+        self.name = name
         self._storage = storage
         self._mode = mode
         self._is_dirty = False
-        self.file = BytesIO()
+        self._file = None
+
+    def _get_file(self):
+        if self._file is None:
+            data = self._storage._read(self.name)
+            self._file = BytesIO(data)
+        return self._file
+
+    def _set_file(self, value):
+        self._file = value
+
+    file = property(_get_file, _set_file)
 
     @property
     def size(self):
         if not hasattr(self, '_size'):
-            self._size = self._storage.size(self._name)
+            self._size = self._storage.size(self.name)
         return self._size
 
     def read(self, num_bytes=None):
-        data = self._storage._read(self._name)
-        self.file = BytesIO(data)
         return self.file.read(num_bytes)
 
     def write(self, content):
@@ -165,5 +174,5 @@ class LibCloudFile(File):
 
     def close(self):
         if self._is_dirty:
-            self._storage._save(self._name, self.file)
+            self._storage._save(self.name, self.file)
         self.file.close()
