dfa8e3947a7f631e8b7ee7a74490acebd6f650f6
Josh Schneier <josh.schneier@gmail.com>
2015-08-14 17:42:29 -0400

    add failing test case for #63

--- tests/test_s3boto.py
+++ tests/test_s3boto.py
@@ -1,3 +1,4 @@
+import unittest
 try:
     from unittest import mock
 except ImportError:  # Python 3.2 and below
@@ -7,6 +8,7 @@ import datetime
 
 from django.test import TestCase
 from django.core.files.base import ContentFile
+import django
 
 from boto.s3.key import Key
 from boto.utils import parse_ts, ISO8601
@@ -295,3 +297,9 @@ class S3BotoStorageTests(S3BotoTestCase):
             self.storage.save(name, content)
             self.assertEqual(self.storage.modified_time(name),
                              parse_ts(utcnow.strftime(ISO8601)))
+
+    @unittest.skipIf(django.VERSION >= (1, 8), 'Only test backward compat of max_length for versions before 1.8')
+    def test_max_length_compat_okay(self):
+        self.storage.file_overwrite = False
+        self.storage.exists = lambda name: False
+        self.storage.get_available_name('gogogo', max_length=255)
