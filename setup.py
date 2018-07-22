38f03a97f34b5cc54f18f11c995d12380fc5041c
Akshay Raj Gollahalli <akshaybabloo@gmail.com>
2018-07-22 21:08:17 +1200

    updated setup and tox

--- setup.py
+++ setup.py
@@ -8,15 +8,20 @@ def read(filename):
         return f.read()
 
 
-def get_requirements_tests():
-    with open('requirements-tests.txt') as f:
-        return f.readlines()
-
-
 setup(
     name='django-storages',
     version=storages.__version__,
     packages=['storages', 'storages.backends'],
+    install_requires=['Django>=1.11'],
+    extras_require={
+        'azure': ['azure'],
+        'boto': ['boto>=2.32.0'],
+        'boto3': ['boto3>=1.2.3'],
+        'dropbox': ['dropbox>=7.2.1'],
+        'google': ['google-cloud-storage>=0.22.0'],
+        'libcloud': ['apache-libcloud'],
+        'sftp': ['paramiko'],
+    },
     author='Josh Schneier',
     author_email='josh.schneier@gmail.com',
     license='BSD',
@@ -27,9 +32,9 @@ setup(
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
         'Framework :: Django',
-        'Framework :: Django :: 1.8',
-        'Framework :: Django :: 1.10',
         'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
@@ -37,12 +42,11 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
-    tests_require=get_requirements_tests(),
     test_suite='tests',
     zip_safe=False
 )
