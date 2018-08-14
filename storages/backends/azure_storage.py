0f4b544094067659bd0ef589de871c915034660f
Roman Vorobyov <roman.vorobyov@xpo.com>
2018-08-14 14:53:38 -0400

    Cleanup/refactor code to remove legacy code for supporting old versions of azure classes (i.e. pre BlockBlobService)

--- storages/backends/azure_storage.py
+++ storages/backends/azure_storage.py
@@ -1,9 +1,5 @@
 import mimetypes
-import os.path
 import posixpath
-import time
-from datetime import datetime
-from time import mktime
 
 from django.core.exceptions import ImproperlyConfigured, SuspiciousOperation
 from django.core.files.base import ContentFile
@@ -13,23 +9,11 @@ from django.utils.deconstruct import deconstructible
 from storages.utils import safe_join, setting
 
 try:
-    import azure  # noqa
+    from azure.storage.blob import BlockBlobService, ContentSettings
 except ImportError:
     raise ImproperlyConfigured(
         "Could not load Azure bindings. "
         "See https://github.com/WindowsAzure/azure-sdk-for-python")
-
-if (setting("AZURE_2018_SDK")):
-    from azure.storage.blob import BlockBlobService, ContentSettings
-    from azure.common import AzureMissingResourceHttpError
-else:
-    try:
-        # azure-storage 0.20.0
-        from azure.storage.blob.blobservice import BlobService
-        from azure.common import AzureMissingResourceHttpError
-    except ImportError:
-        from azure.storage import BlobService
-        from azure import WindowsAzureMissingResourceError as AzureMissingResourceHttpError
 
 
 @deconstructible
@@ -41,17 +25,14 @@ class AzureStorage(Storage):
     azure_location = setting("AZURE_LOCATION", "")
 
     def __init__(self, *args, **kwargs):
-        super(AzureStorage, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
         self._connection = None
         self.azure_location = (self.azure_location or '').lstrip('/')
 
     @property
     def connection(self):
         if self._connection is None:
-            if (setting("AZURE_2018_SDK")):
-                self._connection = BlockBlobService(account_name=self.account_name, account_key=self.account_key)
-            else:
-                self._connection = BlobService(self.account_name, self.account_key)
+            self._connection = BlockBlobService(account_name=self.account_name, account_key=self.account_key)
         return self._connection
 
     @property
@@ -60,7 +41,7 @@ class AzureStorage(Storage):
             return 'https'
         return 'http' if self.azure_ssl is not None else None
 
-    def _open(self, name, mode="rb"):
+    def _open(self, name, mode=None):
         name = self._normalize_name(self._clean_name(name))
         contents = self.connection.get_blob_to_bytes(self.azure_container, name).content
         return ContentFile(contents)
@@ -73,12 +54,10 @@ class AzureStorage(Storage):
         name = self._normalize_name(self._clean_name(name))
         return self.connection.delete_blob(self.azure_container, name)
 
-
     def size(self, name):
         name = self._normalize_name(self._clean_name(name))
-        properties = self.connection.get_blob_properties(
-            self.azure_container, name)
-        return properties["content-length"]
+        properties = self.connection.get_blob_properties(self.azure_container, name).properties
+        return properties.content_length
 
     def _save(self, name, content):
         name = self._normalize_name(self._clean_name(name))
@@ -92,35 +71,23 @@ class AzureStorage(Storage):
         else:
             content_data = content.read()
 
-        if (setting("AZURE_2018_SDK")):
-            self.connection.create_blob_from_bytes(self.azure_container, name,
-                                 content_data, content_settings=ContentSettings(content_type=content_type))
-        else:
-            self.connection.put_blob(self.azure_container, name,
-                                 content_data, "BlockBlob", x_ms_blob_content_type=content_type)
+        self.connection.create_blob_from_bytes(
+            self.azure_container,
+            name,
+            content_data,
+            content_settings=ContentSettings(content_type=content_type)
+        )
 
         return name
 
     def url(self, name):
         name = self._normalize_name(self._clean_name(name))
-        if hasattr(self.connection, 'make_blob_url'):
-            return self.connection.make_blob_url(
-                container_name=self.azure_container,
-                blob_name=name,
-                protocol=self.azure_protocol,
-            )
-        else:
-            return "{}{}/{}".format(setting('MEDIA_URL'), self.azure_container, name)
+        return self.connection.make_blob_url(self.azure_container, name, protocol=self.azure_protocol)
 
     def get_modified_time(self, name):
         name = self._normalize_name(self._clean_name(name))
-        try:
-            prop = self.connection.get_blob_properties(self.azure_container, name)
-            modified = prop.properties.last_modified
-        except (TypeError, KeyError):
-            return super(AzureStorage, self).modified_time(name)
-
-        return modified
+        properties = self.connection.get_blob_properties(self.azure_container, name).properties
+        return properties.last_modified
 
     def _clean_name(self, name):
         """
