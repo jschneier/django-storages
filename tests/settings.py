2aa1e71bbfc48fc8221c2085a03b5886e061f1e2
Jon Dufresne <jon.dufresne@gmail.com>
2018-09-06 19:07:35 -0700

    Remove unnecessary settings from tests/settings.py
    Backend settings should be set as necessary in tests; not globally.

--- tests/settings.py
+++ tests/settings.py
@@ -1,13 +1,4 @@
-import os
-
-MEDIA_ROOT = os.path.normcase(os.path.dirname(os.path.abspath(__file__)))
 MEDIA_URL = '/media/'
-
-INSTALLED_APPS = (
-    'django.contrib.auth',
-    'django.contrib.sessions',
-    'django.contrib.contenttypes',
-)
 
 DATABASES = {
     'default': {
@@ -16,17 +7,6 @@ DATABASES = {
     }
 }
 
-MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-)
-
-DEFAULT_FILE_STORAGE = 'backends.s3boto.S3BotoStorage'
-AWS_IS_GZIPPED = True
-GS_IS_GZIPPED = True
 SECRET_KEY = 'hailthesunshine'
 
 USE_TZ = True
-TIME_ZONE = 'America/Chicago'
