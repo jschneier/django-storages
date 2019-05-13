d9a0c4786bb0149ccc9e13a2e7bd2c27cb65fe28
ViNS <vins@krypton>
2019-05-13 06:36:30 -0700

    Fixed Flake8 errors

--- tests/test_gcloud.py
+++ tests/test_gcloud.py
@@ -131,7 +131,8 @@ class GCloudStorageTests(GCloudTestCase):
 
         self.storage._client.get_bucket.assert_called_with(self.bucket_name)
         self.storage._bucket.get_blob().upload_from_file.assert_called_with(
-            content, rewind=True, size=len(data), content_type=mimetypes.guess_type(filename)[0], predefined_acl='publicRead')
+            content, rewind=True, size=len(data), content_type=mimetypes.guess_type(filename)[0],
+            predefined_acl='publicRead')
 
     def test_delete(self):
         self.storage.delete(self.filename)
