a5098abfe54908fcabff484e3cbb4d9c2fada53f
mcueto <cueto@live.cl>
2019-06-10 03:41:13 -0400

    Bugfix: set default_acl as 'public-read'

--- storages/backends/do_spaces.py
+++ storages/backends/do_spaces.py
@@ -20,13 +20,13 @@ class DigitalOceanSpacesStorage(S3Boto3Storage):
 
 class DigitalOceanSpacesStaticStorage(DigitalOceanSpacesStorage):
     location = settings.DO_SPACES_STATIC_LOCATION
-    default_acl = 'public'
+    default_acl = 'public-read'
 
 
 class DigitalOceanSpacesPublicMediaStorage(DigitalOceanSpacesStorage):
     location = settings.DO_SPACES_PUBLIC_MEDIA_LOCATION
     file_overwrite = False
-    default_acl = 'public'
+    default_acl = 'public-read'
 
 
 class DigitalOceanSpacesPrivateMediaStorage(DigitalOceanSpacesStorage):
