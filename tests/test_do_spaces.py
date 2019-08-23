bc5786e32cea6e32530cd5ae08da96b9730466c0
chris wedgwood <chriswedgwood@tfl.gov.uk>
2019-08-23 11:15:51 +0100

    Fixing indentation

--- tests/test_do_spaces.py
+++ tests/test_do_spaces.py
@@ -16,7 +16,7 @@ from django.test import TestCase, override_settings
 from django.utils.six.moves.urllib import parse as urlparse
 from django.utils.timezone import is_aware, utc
 
-
+from storages.backends import s3boto3
 from storages.backends import do_spaces
 
 try:
