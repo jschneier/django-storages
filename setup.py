c3ab2f272fc636f18f2e22f44e404189bee6fc54
Misha Kalyna <mike.viburn@gmail.com>
2019-10-06 14:25:28 +0200

    removed django 3.0 from setup.py

--- setup.py
+++ setup.py
@@ -37,7 +37,6 @@ setup(
         'Framework :: Django :: 2.0',
         'Framework :: Django :: 2.1',
         'Framework :: Django :: 2.2',
-        'Framework :: Django :: 3.0',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
