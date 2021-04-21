d2cc8efca2012b7fa8ebbdd6c82886398fb519b6
Danny Sauer <danny.sauer@konghq.com>
2021-04-21 12:47:29 -0500

    Set safe chars for apt compatibility
    For some reason, apt insists on reparsing URLs.  So signed CloudFront URLs break when there's a quote, and quotes are supposed to be there in the content-disposition header commonly used with S3 assests stored using not-the-real-filename.

--- storages/backends/s3boto3.py
+++ storages/backends/s3boto3.py
@@ -572,7 +572,7 @@ class S3Boto3Storage(BaseStorage):
                 self._s3_service_model['operations']['GetObject'], sm)
         serializer = botocore.serialize.create_serializer('rest-json')
         url_dict = serializer.serialize_to_request(params, om)
-        return urlencode(url_dict['query_string'])
+        return urlencode(url_dict['query_string'], safe='/",')
 
     def url(self, name, parameters=None, expire=None, http_method=None):
         # Preserve the trailing slash after normalizing the path.
@@ -585,7 +585,7 @@ class S3Boto3Storage(BaseStorage):
         params['Key'] = name
         # The "ResponseExpires" value ends up with a comma, which
         #  often gets un-escaped by some over-zealos URL re-parsers. :/
-        # Changing the signed URL then breaks the signature.
+        # It also doesn't make sense behind CloudFront
         # params['ResponseExpires'] = expiration
 
         if self.custom_domain:
