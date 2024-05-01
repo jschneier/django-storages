3b5ba16707f9fe60c72036b85212ee55f50a0b38
Alex Bridge <alex.bridge@torchbox.com>
2024-05-01 11:55:49 +0100

    AzureStorageTest.test_missing_file_handling: use storage's 'open' method, not private '_open'

--- tests/test_azure.py
+++ tests/test_azure.py
@@ -508,7 +508,7 @@ class AzureStorageTest(TestCase):
         client_mock.exists.side_effect = [True, False]
         self.storage._client.get_blob_client.return_value = client_mock
 
-        file_that_exists = self.storage._open('file-that-exists')
+        file_that_exists = self.storage.open('file-that-exists')
         self.assertIsInstance(file_that_exists, azure_storage.AzureStorageFile)
 
-        self.assertRaises(FileNotFoundError, self.storage._open, 'file-that-does-not-exist')
+        self.assertRaises(FileNotFoundError, self.storage.open, 'file-that-does-not-exist')
